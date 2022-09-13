ccf727e7e30b89d8c04488821b4bd7c1c45b9f96
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-13 20:57:43 +0800

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,33 +1,44 @@
-//global scope
-var a = 1;
-let b = 2;
-const c = 3;
-
-//function scope
-// function test(){
-//     var a = 4;
-//     let b = 5;
-//     const c = 6;
-
-//     console.log('Function scope: ' , a,b,c);
-// }
-
-// test();
-
-// //block level  scope
-if(true){
-    var a = 4;
-    let b = 5 ;
-    const c = 6;
-
-    console.log('Block level scope: ',a,b,c);
-}
+// let , const 
+// How to name a variable 
+// letters , numbers , _ , $
+// variable name can not start with numbers
 
 
+//let keyword 
+let student = 'Sara';
+
+// initialise the variable
+let output;
+
+// let variable can be updated
+//  output = 666;
+//  output = 777;
+
+// let variable canot be redeclared in the same scope
+//  let output = 666;
+
+console.log(output); /*undefined (uncomment line 13 to define and line
+14 to re-define)*/
+console.log(student);
 
 
+// const  
+const person = 'John';
 
+const value = 555;
 
+/* if this line is executed will get an error because
+const value cannot be re-declared & updated */
+//  person = 'Sam';
+// value= 666;
 
+// const must have value
+// const value;
 
-console.log('Global scope: ' , a,b,c);
+console.log(person);
+console.log(value);
+
+// multi words 
+let firstName = 'Sara';  //camelCase  *
+let LastName = 'Smith';   //PascalCase
+let first_name = 'Doe';   //Underscore
