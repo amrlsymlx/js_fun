a1184c3958a6351eb93dc625474fade37b70fee0
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-13 18:45:40 +0800

    Update let_const_03.js

--- let_const_03.js
+++ let_const_03.js
@@ -1,25 +1,28 @@
-// let , const
-// How to name a varible
+// let , const 
+// How to name a variable 
 // letters , numbers , _ , $
+// variable name can not start with numbers
 
-// let keyword
+
+//let keyword 
 let student = 'Sara';
 student = 'kim';
 
-// initialize the variable
+// initialise the variable
 let output;
 
 console.log(output);
 console.log(student);
 
-// const
+
+// const 
 const person = 'John';
 // const value;
 // person = 'Sam';
-// console.log(value);
 console.log(person);
+// console.log(value);
 
-// multi words
-let firstName = 'Sara'; //camelCase
-let LastName = 'Smith'; //PascalCase
-let fist_name = 'Doe'; //underscore
+// multi words 
+let firstName = 'Sara';  //camelCase  *
+let LastName = 'Smith';   //PascalCase
+let first_name = 'Doe';   //Underscore
