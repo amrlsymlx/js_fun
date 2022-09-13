7c6b2f9c645143a5a0061a03f3164c09fcb59a27
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-13 23:04:29 +0800

    new

--- objects_literal_10.js
+++ objects_literal_10.js
@@ -1,36 +1,36 @@
-
 const person = {
-    firstName: 'Sara',
-    lastName: 'Smith',
-    age: 30,
-    email: 'sara@gmail.com',
-    hobbies: ['music', 'sport'],
+    firstName:'sara',
+    lastName:'Smith',
+    age:30,
+    email:'sara@gmail.com',
+    hobbies:['music','sport'],
     address: {
-        city: 'KL',
-        country: 'Malaysia'
+        city:'KL',
+        country:'Malaysia'
     }
 }
 
 let val;
 
-// Get specific value
+//get the specific value
 val = person.age;
 val = person['lastName'];
 val = person.firstName;
-val = person.hobbies;
+val= person.hobbies;
 val = person.hobbies[0];
 val = person.hobbies[1];
 val = person.address;
-val = person.address.city
+val= person.address.city;
 val = person.address.country;
-val = person.email;
+val= person.email;
+
 
 // console.log(val);
 
 const people = [
-    {userName: 'Sara', age: 30},
-    {userName: 'Kim', age: 35},
-    {userName: 'John', age: 40},
+    {userName:'sara',age:30},
+    {userName:'kim',age:35},
+    {userName:'John',age:40},
 ];
 
 console.log(people);
@@ -38,6 +38,7 @@ val = people[0].userName;
 
 // console.log(val);
 
-for (let i = 0; i < people.length; i++) {
+
+for(let i = 0 ; i < people.length  ; i++){
     console.log(people[i].age);
 }
--- practice.js
+++ practice.js
@@ -1,53 +1,29 @@
-// Create some arrays
-const numbers = [43,65,77,100,1,23];
-const numbers2=new Array(22,44,55,66,77);
-const fruits = ['Apple','Orange','Kiwi','Peach'];
-const mix = ['hello',3,true,undefined,{a:1,b:2}];
-
-let val;
-
-
-//add on to the end
-numbers.push(777);
-
-//add to the begining 
-numbers.unshift(55);
-
-//remove from the end
-numbers.pop();
-
-//remove from the begining
-numbers.shift();
-
-//splice
-// numbers.splice(1,4);
-
-//reverse
-// numbers.reverse();
+const person = {
+    firstName:'sara',
+    lastName:'Smith',
+    age:30,
+    email:'sara@gmail.com',
+    hobbies:['music','sport'],
+    address: {
+        city:'KL',
+        country:'Malaysia'
+    }
+}
 
 
-// console.log(numbers);
 
+const people = [
+    {userName:'sara',age:30},
+    {userName:'kim',age:35},
+    {userName:'John',age:40},
+];
 
-//concat
-// val = numbers.concat(numbers2);
-
-//sort arrays
-// val = fruits.sort();
-// val = numbers.sort();
-
-// val = numbers.sort(function(x,y){
-//     return x-y;
-//  });
-
-val = numbers.sort(function(x,y){
-    return y-x
-});
-
-// function over50(num){
-//     return num >50;
-// };
-
-// val = numbers.find(over50);
+console.log(people);
+val = people[0].userName;
 
 console.log(val);
+
+
+// for(let i = 0 ; i < people.length  ; i++){
+//     console.log(people[i].age);
+// }
