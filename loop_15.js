a5b6e5d3c47cee7869a7c08f3da00fcb2909e771
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-17 18:45:55 +0800

    new

--- loop_15.js
+++ loop_15.js
@@ -1,62 +1,60 @@
-// For loop
+//for loop
 
-// for(let i = 1; i <= 10; i++) {
-//     if(i === 2) {
+// for(let i = 1 ; i <= 10 ; i++){
+//     if(i === 2){
 //         console.log('id found');
 //         continue;
 //     }
-//     if(i === 5) {
-//         console.log('stop the loop')
+//     if(i === 5){
+//         console.log('stop the loop');
 //         break;
 //     }
 //     console.log(i);
 // }
 
-// While loop
+// while loop
 
 // let i = 0;
 
-// while(i < 10) {
-
+// while(i  <= 10){
 //     console.log(i);
 //     i++;
 // }
 
-//Do wile
+//Do while
 
-// let i = 0
+//  let i = 1;
 
-// do {
+//  do {
 //     console.log(i);
 //     i++
 // }
 
-// while(i < 10);
+// while(i<=10);
 
 //loop an array
-const numbers = [22, 33, 44, 55, 66, 77, 88, 99];
+const numbers = [22,33,44,55,66,77,88,99];
 
-// for (let i = 0; i < numbers.length; i++) {
+// for (let i = 0 ; i < numbers.length ; i++){
 //     console.log(i, numbers[i]);
 // }
 
-// forEach (its only for arrays)
-
-// numbers.forEach(function(num, index) {
-//     console.log(index, num)
+//forEach (its only for arrays)
+// numbers.forEach(function(num,index){
+//     console.log(index,num)
 // });
 
 
 const users = [
-    {name: 'Sara', id:1},
-    {name: 'Kim', id:2},
-    {name: 'John', id:3},
+    {name:'sara',id:1},
+    {name:'kim',id:2},
+    {name:'john',id:3},
 ];
 
-// for(let i = 0; i < users.length; i++) {
-//     console.log(users[i].name);
-// }
+for(let i = 0 ; i < users.length ; i++){
+    console.log(users[i].name);
+}
 
-users.forEach(function(user) {
-    console.log(user.name)
+users.forEach(function(user){
+    console.log(user.name);
 })
--- practice.js
+++ practice.js
@@ -1,41 +1,60 @@
-//Function declaration
+//for loop
 
-function greet(firstName='John',lastName='Doe'){
-    //   if(typeof firstName === 'undefined'){firstName='John'}
-    //   if(typeof lastName === 'undefined'){lastName='Doe'}
-    
-        console.log(`welcome ${firstName} ${lastName}`);
-    } 
-    greet();
-    
-    //function expression
-    
-    const square = function(x=3){
-        return x*x;
-    };
-    console.log(square(5));
-    
-    //Immidiate invokable function expression
-    (function(){
-        console.log('IIFE RAN...')
-    })();
-    
-    (function(userName='John'){
-        console.log(`welcome ${userName}`)
-    })();
-    
-    // property method
-    //when a function is a property of an object its 
-    // called method 
-    
-    const todo= {
-        add : function(){
-            console.log('add to the list');
-        },
-        edit : function(){
-            console.log('edit the  list');
-        }
-    }
-    
-    todo.add();
-    todo.edit();
+// for(let i = 1 ; i <= 10 ; i++){
+//     if(i === 2){
+//         console.log('id found');
+//         continue;
+//     }
+//     if(i === 5){
+//         console.log('stop the loop');
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// while loop
+
+// let i = 0;
+
+// while(i  <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+//Do while
+
+//  let i = 1;
+
+//  do {
+//     console.log(i);
+//     i++
+// }
+
+// while(i<=10);
+
+//loop an array
+const numbers = [22,33,44,55,66,77,88,99];
+
+// for (let i = 0 ; i < numbers.length ; i++){
+//     console.log(i, numbers[i]);
+// }
+
+//forEach (its only for arrays)
+// numbers.forEach(function(num,index){
+//     console.log(index,num)
+// });
+
+
+const users = [
+    {name:'sara',id:1},
+    {name:'kim',id:2},
+    {name:'john',id:3},
+];
+
+for(let i = 0 ; i < users.length ; i++){
+    console.log(users[i].name);
+}
+
+users.forEach(function(user){
+    console.log(user.name);
+})
